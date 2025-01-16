4e718b63136c0fee8ff245b0135b4b67bc8d06ef
Vyshakh6547 <vyshakh6547@gmail.com>
2025-01-16 02:03:58 -0800

    Update fib.cpp

--- Labs/Lab1/Test_1234/fib.cpp
+++ Labs/Lab1/Test_1234/fib.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int fib(int n) {
   if (n <= 1) return 1;
   return fib(n - 1) + fib(n - 2);
 }
+
 int main() {
   std::cout << fib(0) << " " << fib(1) << " " << fib(2) << " " << fib(3) << " " << fib(4) << " " << fib(5) << " " << fib(6) << " " << fib(7) << " " << fib(8)
    << endl;
+   vector<pair<string, string>> dictionary;
+   dictionary.push_back(std::make_pair("hello", "world"));
+   dictionary.push_back(std::make_pair("why", "not"));
+   dictionary.push_back(std::make_pair("hi", "there"));
+  for (const auto& entry : dictionary) {
+      cout << entry.first << ": " << entry.second << endl;
+  }
   return 0;
 }
