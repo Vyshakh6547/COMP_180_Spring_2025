5f4bc0b608ac4a8fd0f47ec5c1a6cf08ccb39cf2
shradha_101 <48127159+Shradhapujari@users.noreply.github.com>
2025-01-22 10:21:15 +0530

    Update main.cpp

--- Labs/Lab2/ShradhaP_3156/main.cpp
+++ Labs/Lab2/ShradhaP_3156/main.cpp
@@ -1,5 +1,10 @@
+/* Shradha D Pujari
+COMP-180-01
+Lab 2: User-Defined Functions Lab
+*/
+
+#include <iostream>
 #include <vector>
-#include<iostream>
 
 using namespace std;
 
@@ -7,70 +12,37 @@ int ProcessQueue(int total, int current, char oper)
 {
     if (oper == '+') return total + current;
     if (oper == '-') return total - current;
-    if (oper == '/') return total / current;
     if (oper == '*') return total * current;
-    if (oper == '=' && current != 0) return current;
-    if (oper == ' ') return total;
-
+    if (oper == '/') {
+        if (current == 0) {
+            cout << "Error" << endl;
+            return total;
+        }
+        return total / current;
+    }
+    return total;
 }
 
-int main() {
+int main() 
+{
     vector<char> opQueue;
     int totalValue = 0, currValue = 0;
-    char input, queue = ' ';
-    opQueue.push_back(input);
-    totalValue = ProcessQueue(totalValue, currValue, queue);
-    for (auto op : opQueue) {
-        cout << op;
-    }
+    char input;
+
     do {
         cout << "Total Value =" << totalValue << endl;
         cout << "Current Value = " << currValue << endl;
         cin >> input;
-        if (input >= '0' && input <= '9') {
-            currValue *= 10;
-            currValue += (input - '0');
-        }
-        if (input == '+' || input == '-' || input == '*' || input == '/' || input == '=') {
-            if (queue != ' ')
-            {
-                if (queue == '+')
-                {
-                    totalValue += currValue;
-                    currValue = 0;
-                    queue = input;
-                }
-                else if (queue == '-')
-                {
-                    totalValue -= currValue;
-                    currValue = 0;
-                    queue = input;
-                }
-                else if (queue == '*')
-                {
-                    totalValue *= currValue;
-                    currValue = 0;
-                    queue = input;
-                }
-                else if (queue == '/')
-                {
-                    totalValue /= currValue;
-                    currValue = 0;
-                    queue = input;
-                }
-                else if (queue == '=')
-                {
-                    if (currValue != 0) totalValue = currValue;
-                    currValue = 0;
-                    queue = input;
-                }
-            }
-            else {
-                totalValue = currValue;
-                currValue = 0;
-                queue = input;
-            }
 
+        if (input >= '0' && input <= '9')
+        {
+            currValue = currValue * 10 + (input - '0');
+        }
+        else if (input == '+' || input == '-' || input == '*' || input == '/' || input == '=')
+        {
+            opQueue.push_back(input);
+            totalValue = ProcessQueue(totalValue, currValue, opQueue.back());
+            currValue = 0;
         }
 
     } while (input != 'q');
